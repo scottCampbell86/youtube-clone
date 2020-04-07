c637f5f041d86326559af53645d96f2ab2bd685b
Scott Campbell <stc9486@gmail.com>
2020-04-07 16:51:53 -0700

    search

--- react-youtube-clone/src/components/SearchBar.js
+++ react-youtube-clone/src/components/SearchBar.js
@@ -2,13 +2,38 @@ import React, { Component } from 'react';
 import '../style/SearchBar.css'
 
 class SearchBar extends Component {
+
+  state = { term: ''}
+
+  handleInput = (e) => {
+    //e.preventDefault()
+    this.setState({
+      term: e.target.value
+    })
+  }
+
+  handleSubmite = (e) => {
+    e.preventDefault();
+    //invoke callback from props from parent component
+  }
+
+
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form 
+          className="ui form"
+          onSubmit={this.handleSubmit}
+          >
           <div className="field">
             <label>Video Search</label>
-            <input type="text"/>
+            <input 
+              type="text" 
+              value={this.state.term} 
+              onChange={this.handleInput} 
+              //or, onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
