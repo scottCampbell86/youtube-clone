e93ffed5be7bffe66352f8ef2757e83c84debfd3
Scott Campbell <stc9486@gmail.com>
2020-04-07 16:21:03 -0700

    arch

---
+++ react-youtube-clone/src/components/VideoList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../style/VideoList.css'
+
+class VideoList extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default VideoList;
