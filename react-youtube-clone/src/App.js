d0ba7c03fc9a089546e6f4ea0cfc5b233574e64d
Scott Campbell <stc9486@gmail.com>
2020-04-07 16:36:08 -0700

    form

--- react-youtube-clone/src/App.js
+++ react-youtube-clone/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import './App.css'
+import './App.css';
+import SearchBar from './components/SearchBar'
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <SearchBar />
       </div>
     );
   }
--- react-youtube-clone/src/components/SearchBar.js
+++ react-youtube-clone/src/components/SearchBar.js
@@ -4,8 +4,13 @@ import '../style/SearchBar.css'
 class SearchBar extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text"/>
+          </div>
+        </form>
       </div>
     );
   }
