ac7e6705b4b0265da4c6b0e964cfbe56f0e040d9
Scott Campbell <stc9486@gmail.com>
2020-04-07 17:42:14 -0700

    api req

--- react-youtube-clone/src/App.js
+++ react-youtube-clone/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar'
+import SearchBar from './components/SearchBar';
+import youtube from './apis/youtube';
+
+//from api docs: GET https://www.googleapis.com/youtube/v3/search
 
 class App extends Component {
+  handleFormSubmit = (term) => {
+    youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+  }
+  
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar handleFormSubmit={this.handleFormSubmit} />
       </div>
     );
   }
