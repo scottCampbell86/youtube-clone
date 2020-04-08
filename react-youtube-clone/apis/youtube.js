b3e5c805e7c022a6a21124f9b188947514b940a9
Scott Campbell <stc9486@gmail.com>
2020-04-07 17:16:00 -0700

    axios in index

--- react-youtube-clone/apis/youtube.js
+++ react-youtube-clone/apis/youtube.js
@@ -1,1 +1,7 @@
-const KEY = 'AIzaSyA7aaQrepjfXpT13BooEcmmP5ov1oiNS9w'
+import axios from 'axios';
+const KEY = 'AIzaSyA7aaQrepjfXpT13BooEcmmP5ov1oiNS9w';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+});
+
