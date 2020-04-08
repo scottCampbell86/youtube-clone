492d7451b5ed3bfdc5b83500cb70f0b04a29957d
Scott Campbell <stc9486@gmail.com>
2020-04-07 17:17:59 -0700

    youtube.js ~index

--- react-youtube-clone/apis/youtube.js
+++ react-youtube-clone/apis/youtube.js
@@ -3,5 +3,10 @@ const KEY = 'AIzaSyA7aaQrepjfXpT13BooEcmmP5ov1oiNS9w';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
 });
 
